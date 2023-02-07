5102e95ec304f66f4f0258b32a57d4a8b8b9bf79
Josh <jgamlen@ucsc.edu>
2023-02-06 18:20:12 -0800

    part1.2

--- part1/bakery.h
+++ part1/bakery.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+using namespace std;
 
 class mutex {
  public:
@@ -10,16 +11,23 @@ class mutex {
   
   void init (int num_threads) {
     // Implement me!
+    counter = 0;
+    currently_serving = 0;
   }
   
   void lock(int thread_id) {
     // Implement me!
+    int my_num = atomic_fetch_add(&counter, 1);
+    while (currently_serving.load() != my_num) {}
   }
   
   void unlock(int thread_id) {
     // Implement me!
+    currently_serving ++;
   }
 
  private:
   // Give me some private variables!
+  atomic_int counter;
+  atomic_int currently_serving;
 };
