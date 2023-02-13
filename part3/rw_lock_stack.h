2bf7ee12929c3f56aa2074e634a5f031d4fe97df
Josh <jgamlen@ucsc.edu>
2023-02-12 17:48:46 -0800

    3.2

--- part3/rw_lock_stack.h
+++ part3/rw_lock_stack.h
@@ -20,14 +20,19 @@ class CSE113_Stack {
   }
   
   int pop() {
+    //printf("in pop\n");
+    m.lock();
     if (start == NULL) {
+      m.unlock();
       return -1;
     }
 
+    
     if (start->next == NULL) {
       int ret = start->data;
       delete start;
       start = NULL;
+      m.unlock();
       return ret;
     }
 
@@ -41,12 +46,16 @@ class CSE113_Stack {
     int ret = current->data;
     previous->next = NULL;
     delete current;
+    m.unlock();
     return ret;
   }
 
   
   int peek() {
+    //printf("in peek\n");
+    m.lock_shared();
     if (start == NULL) {
+      m.unlock_shared();
       return -1;
     }
     
@@ -54,25 +63,32 @@ class CSE113_Stack {
     while (current->next != NULL) {
       current = current->next;
     }
-    return current->data;    
+    int ret = current->data;
+    m.unlock_shared();
+    return ret;    
   }
   
   void push(int p) {
+    //printf("in push\n");
+    m.lock();
     if (start == NULL) {
-
       start = new Llist_node(p);
+      m.unlock();
       return;
     }
 
+    
     Llist_node *current = start;
     while (current->next != NULL) {
       current = current->next;
     }
 
     current->next = new Llist_node(p);
+    m.unlock();
   }
 
  private:
-  Llist_node *start;  
+  Llist_node *start;
+  shared_mutex m;
 };
 
