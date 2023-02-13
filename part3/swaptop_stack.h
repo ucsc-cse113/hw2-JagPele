705e1b59c5778ef70aa7ab8929123ad03487660e
Josh <jgamlen@ucsc.edu>
2023-02-12 18:01:06 -0800

    3.3

--- part3/swaptop_stack.h
+++ part3/swaptop_stack.h
@@ -20,7 +20,9 @@ class CSE113_Stack {
   }
   
   int pop() {
+    m.lock();
     if (start == NULL) {
+      m.unlock();
       return -1;
     }
 
@@ -28,6 +30,7 @@ class CSE113_Stack {
       int ret = start->data;
       delete start;
       start = NULL;
+      m.unlock();
       return ret;
     }
 
@@ -41,12 +44,15 @@ class CSE113_Stack {
     int ret = current->data;
     previous->next = NULL;
     delete current;
+    m.unlock();
     return ret;
   }
 
   
   int peek() {
+    m.lock_shared();
     if (start == NULL) {
+      m.unlock_shared();
       return -1;
     }
     
@@ -54,13 +60,17 @@ class CSE113_Stack {
     while (current->next != NULL) {
       current = current->next;
     }
-    return current->data;    
+    int ret = current->data;
+    m.unlock_shared();
+    return ret;    
   }
   
   void push(int p) {
+    m.lock();
     if (start == NULL) {
 
       start = new Llist_node(p);
+      m.unlock();
       return;
     }
 
@@ -70,12 +80,43 @@ class CSE113_Stack {
     }
 
     current->next = new Llist_node(p);
+    m.unlock();
   }
 
   void swaptop(int to_swap) {
     // Implement me!
+    m.lock_shared();
+
+    if (start == NULL) {
+      m.unlock_shared();
+      return;
+    }
+    
+    Llist_node *cur = start;
+
+    while(cur->next != NULL) {
+      cur = cur->next;
+    }
+
+    if (cur->data == to_swap) {
+      m.unlock_shared();
+      return;
+    }
+    m.unlock_shared();
+    
+    m.lock();
+    cur =  start;
+    while (cur->next != NULL) {
+      cur = cur->next;
+    }
+    cur->data = to_swap;
+
+    m.unlock();
+    return;
+    
   }
 
  private:
-  Llist_node *start;  
+  Llist_node *start;
+  shared_mutex m;
 };
